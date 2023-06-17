ca7d276372ba2dead5025de2ef91151b2a53c97e
Manuelleon123 <manuel.j.leon.c2@gmail.com>
2023-06-17 18:02:01 -0400

    finalizado ejersicio 2

---
+++ sintax_exercises/practice/ejercicio_2.c
@@ -0,0 +1,27 @@
+/*
+  2. hacer una funcion que diga si un caracter esta en cadena de caracteres 
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char cadena [100], caracter;
+  int count = 0;
+
+  printf("digite una cadena de caracteres: ");
+  gets(cadena);
+  printf("digite un caracter:");
+  scanf("%c", &caracter);
+  
+  for(int i = 0; i < strlen(cadena); i++) {
+    if (cadena[i] == caracter)
+      count++;
+  }
+if (count > 0) {
+  printf("la cadena de caracteres \"%s\" posee el caracter \"%c\"" y se repite %d veces", cadena de caracter, count);
+} else {
+  printf("la cadena de caracteres  \"%s\" no posee el caracter \"%c\"",cadena de caracter);
+}
+return 0;
+}
--- sintax_exercises/practice/ejercisio_1.c
+++ sintax_exercises/practice/ejercisio_1.c
@@ -1,3 +1,26 @@
 /* 
 1. hacer una funcion que diga si un numero es primo
  */
+#include <stdio.h>
+
+int main() {
+  int num;
+  int count = 0;
+
+  printf ("ingrese un numero:");
+  scanf ("%d", &num);
+
+  for (int i = num; i > 0; i--){
+    if (num % i == 0) {
+      count++;
+    }
+  }   
+
+  if (count == 2) {
+    printf ("el numero %d es primo", num);          
+  } else {
+    printf ("el numero %d no es primo", num);
+  }
+
+  return 0;
+}
